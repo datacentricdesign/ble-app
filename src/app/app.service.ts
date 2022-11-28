b6b812ceba13c5adb0f2062f75727269b77fa14e
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2022-11-28 22:17:03 +0100

    adjustment for github deployment

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -9,8 +9,6 @@ export class AppService {
 
   constructor(private http: HttpClient) { }
 
-
-  configUrl = `assets/config.json`;
   private configSettings: any = null;
 
   get settings() {
@@ -19,7 +17,15 @@ export class AppService {
 
   public load(): Promise<any> {
     return new Promise((resolve, reject) => {
-      this.http.get(this.configUrl).subscribe((response: any) => {
+      let configUrl = `assets/config/dev.config.json`
+      if (environment.dwd) {
+        configUrl =  `assets/config/dwd.config.json`
+      } else if (environment.production) {
+        configUrl =  `assets/config/prod.config.json`
+      }
+      console.log(configUrl)
+      console.log(environment)
+      this.http.get(configUrl).subscribe((response: any) => {
         this.configSettings = response;
         resolve(true);
       });
